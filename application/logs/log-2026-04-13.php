abd45580488cf7e9a4d1e4df70c40db7dbff3873
Maxwellhrms <hrd@maxwellpackers.com>
2026-04-17 14:32:51 +0530

    Auto-sync from cPanel: 2026-04-17 14:32:48

---
+++ application/logs/log-2026-04-13.php
@@ -0,0 +1,204 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-04-13 10:48:01 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 11:20:01 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 11:20:50 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 11:21:03 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 11:51:52 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 11:52:21 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 12:14:03 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 12:31:09 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 12:31:34 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 12:31:47 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 12:50:35 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 12:51:33 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 12:52:40 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 13:21:16 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 13:53:56 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /home/maxwellhrms/public_html/application/controllers/Common.php:140) /home/maxwellhrms/public_html/system/core/Common.php 570
+ERROR - 2026-04-13 13:53:56 --> Severity: Compile Error --> Cannot declare class Common, because the name is already in use /home/maxwellhrms/public_html/application/controllers/Common.php 140
+ERROR - 2026-04-13 13:53:56 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /home/maxwellhrms/public_html/application/controllers/Common.php:140) /home/maxwellhrms/public_html/system/core/Common.php 570
+ERROR - 2026-04-13 13:53:56 --> Severity: Compile Error --> Cannot declare class Common, because the name is already in use /home/maxwellhrms/public_html/application/controllers/Common.php 140
+ERROR - 2026-04-13 14:34:44 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 14:35:47 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 14:36:18 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 14:36:27 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 15:03:25 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 15:47:42 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 17:34:08 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
+ERROR - 2026-04-13 17:35:29 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' mxesi_emp_types) > 0' at line 9 - Invalid query: SELECT *
+FROM `maxwell_esi_master`
+WHERE `mxesi_status` = 1
+AND `mxesi_div_id` IS NULL
+AND `mxesi_state_id` IS NULL
+AND `mxesi_branch_id` IS NULL
+AND `mxesi_affect_from` <= '2026-04-13'
+AND `mxesi_affect_to` >= '2026-04-13'
+AND FIND_IN_SET(, mxesi_emp_types) > 0
