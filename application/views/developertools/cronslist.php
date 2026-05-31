6a7caaaf108d615d3f9771a57f5ce912d9f38025
HarishKumar <developerhkumar@gmail.com>
2026-05-31 17:45:33 +0530

    updated code

--- application/views/developertools/cronslist.php
+++ application/views/developertools/cronslist.php
@@ -366,7 +366,8 @@ $(document).on('submit','.runCronForm',function(e){
             if(response.status == true){
                 responseDiv.html(
                     '<div class="alert alert-success">'+
-                        '<strong>'+response.message+'</strong><hr>'+
+                      '<strong>'+response.message+'</strong>'+
+                        (response.description ? '<br><small>'+response.description+'</small>' : '')+
                         '<b>Updated :</b> '+response.data.updated+'<br>'+
                         '<b>Failed :</b> '+response.data.failed+
                     '</div>'
