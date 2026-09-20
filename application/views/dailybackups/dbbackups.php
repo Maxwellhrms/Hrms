462f7c5d2842001589b20ce3cbf9eff69bc186df
Harish Kumar <hrd@maxwellpackers.com>
2026-09-20 17:43:42 +0530

    addedbutton

--- application/views/dailybackups/dbbackups.php
+++ application/views/dailybackups/dbbackups.php
@@ -1,10 +1,3 @@
-<a href="<?php echo base_url('Developertools/take_database_backup'); ?>"
-   target="_blank"
-   class="btn btn-primary"
-   onclick="return confirm('Are you sure you want to take a new database backup?');">
-    <i class="fa fa-database"></i>
-    Take Backup
-</a>
 <!-- Search Filter -->
 <?php $controller->commonFilters(array(
     'fromdatefilter' => 'Y',
@@ -14,6 +7,12 @@
 )); ?>
 <!-- Search Filter -->
 <hr>
-
+<a href="<?php echo base_url('Developertools/take_database_backup'); ?>"
+   target="_blank"
+   class="btn btn-primary"
+   onclick="return confirm('Are you sure you want to take a new database backup?');">
+    <i class="fa fa-database"></i>
+    Take Backup
+</a>
 <!-- /Page Content -->
 </div>
